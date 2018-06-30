be93586db3fb1da2052f6c5058664de2c6391a3d
rreay724 <rreay724@gmail.com>
2018-06-30 19:48:32 -0400

    Project finished.

--- app/src/test/java/com/londonappbrewery/quizzler/ExampleUnitTest.java
+++ app/src/test/java/com/londonappbrewery/quizzler/ExampleUnitTest.java
@@ -12,4 +12,31 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    public static class TrueFalse {
+
+        private int mQuestionID;
+        private boolean mAnswer;
+
+        public TrueFalse(int questionResourceID, boolean trueOrFalse) {
+            mQuestionID = questionResourceID;
+            mAnswer = trueOrFalse;
+        }
+
+        public int getQuestionID() {
+            return mQuestionID;
+        }
+
+        public void setQuestionID(int questionID) {
+            mQuestionID = questionID;
+        }
+
+        public boolean isAnswer() {
+            return mAnswer;
+        }
+
+        public void setAnswer(boolean answer) {
+            mAnswer = answer;
+        }
+    }
 }
